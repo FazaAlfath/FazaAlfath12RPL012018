e5b4ee2364691f66dc4a875b881715cdc191bb46
FazaAlfath <fazaxrpl2018smkrus@gmail.com>
2020-12-04 08:59:46 +0700

    Initial commit

---
+++ app/src/main/java/com/example/fazaalfath12rpl012018/Registrasi.java
@@ -0,0 +1,14 @@
+package com.example.fazaalfath12rpl012018;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Registrasi extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_registrasi);
+    }
+}
